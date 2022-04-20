1514e4089bd14a48fe7372c0c7263cc81e68afd7
Jeremiah Okoduwa <jerryokoduwa@gotmail.co.uk>
2022-04-20 20:06:11 +0100

    Command service created

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        readonly ILogger<PlatformsController> _logger;
+        public PlatformsController(ILogger<PlatformsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            _logger.LogInformation("----Inbound post request---");
+            return Ok("outboud response");
+        }
+    }
+}
